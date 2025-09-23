d6c99147f34902c431d41eae2aecb081477634b6
annabeldornan <Annabel.Dornan@student.norwichuni.ac.uk>
2025-09-23 14:16:13 +0100

    changed bkground colour

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container} // execute code, you need to put it in brackets
+  
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
+
+  const styles = StyleSheet.create({
+    container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "blue"
+  }
+  });
+
+
