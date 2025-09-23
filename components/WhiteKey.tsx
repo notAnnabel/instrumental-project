ebb0f9a3317c877b5c0a8faf26d0c714efdf4d94
annabeldornan <Annabel.Dornan@student.norwichuni.ac.uk>
2025-09-23 14:45:58 +0100

    whitekey

---
+++ components/WhiteKey.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, View } from "react-native";
+
+
+export default function whiteKey() {
+  return (
+    <View
+      style={styles.blackKey} // execute code, you need to put it in brackets
+    >
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+    blackKey: {width: 100,
+    height: 300,
+    borderWidth: 2,
+    borderColor: "black",
+    backgroundColor: "white"
+}
+    
+
+  });
