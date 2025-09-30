2643de99740f1e220e0ad47e297bb1d72bc4d7b3
annabeldornan <Annabel.Dornan@student.norwichuni.ac.uk>
2025-09-30 10:22:11 +0100

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View } from "react-native";
 
-import WhiteKey from "@/components/whiteKey";
+import WhiteKey from "@/components/WhiteKey";
 
 
 export default function Index() {
