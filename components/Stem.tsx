5b5dd30eb6f0430acc512a4046eb486fbdccc7ae
annabeldornan <Annabel.Dornan@student.norwichuni.ac.uk>
2025-10-15 15:54:44 +0100

    aaaaa

--- components/Stem.tsx
+++ components/Stem.tsx
@@ -12,27 +12,37 @@ export default function Stem() {
     oscillator.type = 'triangle'; // sine wave - other options: square, sawtooth, triangle
 
 
-
-    oscillator.frequency.value = 365;
+    oscillator.frequency.value = 2000;
     oscillator.detune.value = 0; // detuner
     gain.gain.value = 0.50; // volume
 
     gain.connect(ctx.destination);
-    oscillator.start()
+    //oscillator.start()
 
     function onValueChange(Value: number) {
+        oscillator.frequency.value = Value;
+        console.log(Value);
+    }
+
+    function onSlidingStart(Value: number){
+        //oscillator.start()
+        new AudioContext();
         oscillator.frequency.value = Value;
     }
 
     return (
         <View><Slider
-            style={{ width: 200, height: 100, transform: [{ rotate: '-90deg' }], left: 50, top: 300 }}
-            minimumValue={360}
-            maximumValue={1000}
+            style={{ width: 300, height: 100, transform: [{ rotate: '-90deg' }], left: 50, top: 300 }}
+            minimumValue={1000}
+            maximumValue={4000}
             minimumTrackTintColor="#8141e9f5"
             maximumTrackTintColor="#4c1f66ff"
             onValueChange={onValueChange}
-        /></View>)
+            onSlidingStart={onSlidingStart}
+        /></View>
+
+        /*<View styles={loadStyles.Head}>
+    </View>*/)
 
 }
 
@@ -48,6 +58,11 @@ function loadStyles() {
             top: -50,
             zIndex: 1,
             left: 100,
+        },
+        Head : {
+            borderRadius: 100,
+            width: 200,
+            height: 200,
         }
     })
-}
+};
