b2c9366e1c0dff447ba3a08ebc2fe0cff43770e6
annabeldornan <Annabel.Dornan@student.norwichuni.ac.uk>
2025-10-21 12:03:56 +0100

    committed error 2

--- components/Stem.tsx
+++ components/Stem.tsx
@@ -1,5 +1,4 @@
 import Slider from '@react-native-community/slider';
-import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { AudioContext, OscillatorNode } from 'react-native-audio-api';
 
@@ -7,37 +6,26 @@ import { AudioContext, OscillatorNode } from 'react-native-audio-api';
 
 export default function Stem() {
 
-    let ctx = new AudioContext();
     const [oscillator, setOscillator]= useState<OscillatorNode>(ctx.createOscillator());
     //const [volume, setVolume]= useState(ctx.createGain()); 
 
     
 
-    function onValueChange(Value: number) {
         oscillator.frequency.value = Value;
 
-    }
 
-    function onSlidingStart(){
         const newOscillator = ctx.createOscillator();
         
-        newOscillator.type = 'triangle'; // sine wave - other options: square, sawtooth, triangle
         
-        newOscillator.frequency.value = 450;
         newOscillator.detune.value = 0; // detuner
         
-        const volume = ctx.createGain();
         
         
-        volume.gain.value = 0.30; // volume
         
-        newOscillator.connect(volume);
         volume.connect(ctx.destination);
         
-        newOscillator.start()
 
         
-        setOscillator(newOscillator);
     }
     function onSlidingComplete(){
         oscillator.stop();
@@ -46,13 +34,11 @@ export default function Stem() {
     
 
     return (
-        <View><Slider
             style={{ width: 300, height: 100, transform: [{ rotate: '-90deg' }], left: 50, top: 300 }}
             minimumValue={450}
             maximumValue={1000}
             minimumTrackTintColor="#8141e9f5"
             maximumTrackTintColor="#4c1f66ff"
-            onValueChange={onValueChange}
             onSlidingStart={onSlidingStart}
             onSlidingComplete={onSlidingComplete}
             trackHeight={50}
@@ -61,7 +47,6 @@ export default function Stem() {
             thumbTintColor='#faffe4f5'
             //trackStyle={{loadStyles.Stem}}
         />
-        <img src={'../assets/images/otamatone-closed.png'}></img>
         </View>
         
 
